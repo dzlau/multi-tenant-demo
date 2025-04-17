55262974cb467cf9a3b55cb354a6d20f39caba58
Dennis <dennislzf@gmail.com>
2025-04-17 11:34:28 -0700

    added asda

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ import type { NextRequest } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
     console.log('Middleware triggered')
+    console.log(new URL(`/${request.nextUrl.hostname}`, process.env.VERCEL_URL))
     return NextResponse.rewrite(new URL(`/${request.nextUrl.hostname}`, process.env.VERCEL_URL))
 }
 
