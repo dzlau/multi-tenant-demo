fe764681fba9377cc8ef335dfbaf146bc5358547
Dennis <dennislzf@gmail.com>
2025-04-17 12:22:38 -0700

    added asda

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,8 @@ import { get } from '@vercel/edge-config';
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
     console.log('Middleware triggered')
-    const hostname = request.nextUrl.hostname.replace('.', '-')
-    console.log('Host name:', hostname)
-    const hostID = await get(request.nextUrl.hostname);
+    const hostname = request.nextUrl.hostname.replaceAll('.', '-')
+    const hostID = await get(hostname);
     console.log('Host ID:', hostID)
     return NextResponse.rewrite(new URL(`/${hostID}`, process.env.VERCEL_URL))
 }
