a79fa26cccec6292f3b53c706bdabc822f15b7c2
Dennis <dennislzf@gmail.com>
2025-05-02 11:45:22 -0700

    fixed middleware to not trigger if not on prod

--- middleware.ts
+++ middleware.ts
@@ -4,26 +4,31 @@ import { getIdFromHostname } from '@/lib/redis';
 const isOnboardRoute = createRouteMatcher(['/onboard(.*)',])
 const isProtectedRoute = createRouteMatcher(['/dashboard(.*)',])
 export default clerkMiddleware(async (auth, request) => {
-    const { userId, sessionClaims, redirectToSignIn } = await auth()
+    // if user is on production url, we need to check auth and make sure redirect is correct
+    console.log('request.nextUrl.hostname', request.nextUrl.hostname)
+    if (process.env.VERCEL_PROJECT_PRODUCTION_URL === request.nextUrl.hostname) {
+        const { userId, sessionClaims, redirectToSignIn } = await auth()
 
-    // If the user isn't signed in and the route is private, redirect to sign-in
-    if (!userId && isProtectedRoute(request)) return redirectToSignIn({ returnBackUrl: request.url })
+        // If the user isn't signed in and the route is private, redirect to sign-in
+        if (!userId && isProtectedRoute(request)) return redirectToSignIn({ returnBackUrl: request.url })
 
-    // if user is onboarding or in protected route, dont redirect
-    if (userId && (isOnboardRoute(request) || isProtectedRoute(request))) {
-        return NextResponse.next()
-    }
-    // Catch users who do not have `onboardingComplete: true` in their publicMetadata
-    // Redirect them to the /onboarding route to complete onboarding
-    if (userId && !sessionClaims?.metadata?.onboardingComplete) {
-        const onboardingUrl = new URL('/onboard', request.url)
-        return NextResponse.redirect(onboardingUrl)
+        // if user is onboarding or in protected route, dont redirect
+        if (userId && (isOnboardRoute(request) || isProtectedRoute(request))) {
+            return NextResponse.next()
+        }
+        // Catch users who do not have `onboardingComplete: true` in their publicMetadata
+        // Redirect them to the /onboarding route to complete onboarding
+        if (userId && !sessionClaims?.metadata?.onboardingComplete) {
+            const onboardingUrl = new URL('/onboard', request.url)
+            return NextResponse.redirect(onboardingUrl)
+        }
+
+        // check if user is authenticated and has onboardingComplete
+        if (userId && sessionClaims?.metadata?.onboardingComplete && request.nextUrl.pathname === '/') {
+            return NextResponse.redirect(new URL('/dashboard', request.url))
+        }
     }
 
-    // check if user is authenticated and has onboardingComplete
-    if (userId && sessionClaims?.metadata?.onboardingComplete && request.nextUrl.pathname === '/') {
-        return NextResponse.redirect(new URL('/dashboard', request.url))
-    }
 
 
     console.log('Middleware triggered')
@@ -35,7 +40,7 @@ export default clerkMiddleware(async (auth, request) => {
         return
     }
 
-    // if production env, utilize production url
+    // if production env, utilize production url, if not use preview or dev url
     const baseUrl = process.env.VERCEL_ENV === 'production' ? `${request.nextUrl.protocol}//${process.env.VERCEL_PROJECT_PRODUCTION_URL}` : `${request.nextUrl.protocol}//${process.env.VERCEL_URL}`
 
     // if no hostID found, return base route
